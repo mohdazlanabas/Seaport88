e72b8d3b4c9c9b7c4fe0605bdb24f220cad76a9d
Azlan Abas <95009591+mohdazlanabas@users.noreply.github.com>
2022-09-26 20:08:12 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ function getLocation() {
 }
 
 // Set the date we're counting down to
-var countDownDate = new Date("June  23, 2023 00:00:00").getTime();
+var countDownDate = new Date("June 17, 2023 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
