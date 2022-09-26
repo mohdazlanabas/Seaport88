2f5e47052b3a50b4082287b5a6a54f662513abd1
Azlan Abas <mohdazlan.abas@gmail.com>
2022-09-26 17:35:52 +0800

    Update

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 alert("Hello! Seaportian 88 | PENGUMUMAN | ANNOUNCEMENT | 公告 | அறிவிப்பு<");
+
+var datetime = new Date();
+console.log(datetime);
+document.getElementById("time").textContent = datetime; //it will print on html page
+
+var x = document.getElementById("demo");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
