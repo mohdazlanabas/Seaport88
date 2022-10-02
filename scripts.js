32b7b666bfc94f48921a50672a112c69d54fbae1
Azlan Abas <mohdazlan.abas@gmail.com>
2022-10-02 23:18:46 +0800

    update

--- scripts.js
+++ scripts.js
@@ -41,3 +41,12 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+const setBg = () => {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.backgroundColor = "#" + randomColor;
+  // color.innerHTML = "#" + randomColor;
+}
+
+genNew.addEventListener("click", setBg);
+setBg();
